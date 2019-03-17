167caf3877383d280ec694760a609e113f14a332
Prestegaard <hakonp@gmail.com>
2019-03-17 23:57:42 +0100

    Updated mqtt a bit

--- mqtt-remote-control/mqtt.py
+++ mqtt-remote-control/mqtt.py
@@ -1,4 +1,4 @@
-import paho.mqtt.client as mqtt #import the client1
+import paho.mqtt.client as mqtt #import the client1 import time import subprocess
 import time
 import subprocess
 ############
@@ -9,54 +9,47 @@ def on_connect(client, userdata, flags, rc):
     global flag_connected
     flag_connected = 1
     print("Connected to broker")
+    print("Connected with result code "+str(rc))	
 
 
 def on_message(client, userdata, message):
     print("#### New message ####")
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    #print("message qos=",message.qos)
-    #print("message retain flag=",message.retain)
-    
+    print("message topic: ", message.topic)   
+    print("message payload: " ,str(message.payload.decode("utf-8")))
+
     if message.topic == "/NAD/":
         cmd = "irsend SEND_ONCE NAD "
         cmd += message.payload.decode("utf-8")
         cmd = cmd.split()   
+        print("Calling subprocess: ", cmd)
         subprocess.call(cmd)
 
-#POWER_OFF
-#POWER_ON
-#VOLUME+
-#VOLUME-
-#MUTE
-#<a href="/send/NAD/INPUT_CHROMECAST" class="btn btn-primary">Ccast</a>
-#<a href="/send/NAD/INPUT_PI" class="btn btn-primary">Wii U</a>
-#<a href="/send/NAD/INPUT_SOFA" class="btn btn-primary">Kabel</a>
+
+
 ########################################
 broker_address="192.168.1.17"
-#broker_address="iot.eclipse.org"
 print("creating new instance")
-client = mqtt.Client("NAD") #create new instance
-#Client(client_id=””, clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
+
+#client = mqtt.Client() #create new instance
+client = mqtt.Client(client_id="NAD", clean_session=True, userdata=None, protocol=mqtt.MQTTv31, transport="tcp")
 
 client.on_connect = on_connect
 client.on_message = on_message #attach function to callback
-print("connecting to broker")
-with open(password.txt, 'r') as pwfile
-password = pwfile.readline() 
-client.username_pw_set(username='haagon', password=password)
+
+
+mqtt_password = ''
+with open('password.txt', 'r') as pwfile:
+	mqtt_password = pwfile.readline().strip()
+#print("Read MQTT Password from file: {}".format(mqtt_password))
+
+client.username_pw_set("haagon", str(mqtt_password))
+print("Connecting to broker on ip: ", broker_address)
 client.connect(broker_address) #connect to broker
-#connect(host, port=1883, keepalive=60, bind_address="")
 time.sleep(2)
 
-print("Subscribing to topic","/NAD/")
+print("Subscribing to topic: /NAD/")
 client.subscribe("/NAD/")
 time.sleep(2)
 
 print("Loop forever")
 client.loop_forever()
-#client.loop_start() #start the loop
-#print("Publishing message to topic","house/bulbs/bulb1")
-#client.publish("house/bulbs/bulb1","OFF")
-#time.sleep(10) # wait
-#client.loop_stop() #stop the loop
